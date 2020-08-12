b9dd8416ef5cd9cae82c0832e30c690da32c1bf7
CaptainPickles <levesque.lucas@outlook.fr>
2020-08-12 06:04:05 -0500

    ajout d'une fonction qui rajoute 100 titres d'article wikipedia aléatoire

--- wikipedia_se/titre_wiki/views.py
+++ wikipedia_se/titre_wiki/views.py
@@ -4,10 +4,11 @@ import wikipedia
 # Create your views here.
 
 def index(request):
-    ny = wikipedia.page("New York City")
-    context = {
-        "titre" : ny.title,
-        "content" : ny.content
-    }
+    wikipedia.set_lang("fr")
+    titreArticle = []
+    for i in range(1,100):
+        titreArticle.append(wikipedia.random(pages=1))
+    context = { "titre" : titreArticle}
+
     
     return render(request ,'home.html' , context)
